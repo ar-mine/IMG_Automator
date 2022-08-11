e158bfb3e878007f48fb9173c8b018cf0767f690
ar-mine <934218777@qq.com>
2022-08-11 21:07:28 +0800

    Add making template function

--- U2Net/u2net_wrapper.py
+++ U2Net/u2net_wrapper.py
@@ -4,7 +4,7 @@ import sys
 
 import numpy as np
 import torch
-from PIL import Image
+from PIL import Image, ImageFilter
 from skimage import io
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
@@ -31,7 +31,44 @@ def normPRED(d):
     return dn
 
 
-def save_output(image_name, pred, d_dir):
+def save_output_overlay(image_name, mask, d_dir, o_dir):
+    # Convert predicted segmentation mask to numpy array
+    mask_ = mask
+    mask_ = mask_.squeeze()
+    mask_np = mask_.cpu().data.numpy()
+
+    # Segment the image with mask while using black bg(0, 0, 0)
+    image_np = io.imread(image_name)
+    image_ = Image.fromarray(image_np).convert('RGB')
+    background = Image.fromarray(np.zeros((image_np.shape[0], image_np.shape[1]))).convert('RGB')
+    img = Image.fromarray(mask_np * 255).convert('L')
+    img_mask = img.resize((image_np.shape[1], image_np.shape[0]), resample=Image.Resampling.BILINEAR).\
+        convert('1').filter(ImageFilter.MedianFilter(5))
+    img_composite = Image.composite(image_, background, img_mask)
+
+    # Remove path prefix
+    image_name = image_name.split(os.sep)[-1]
+    # left, top, right, bottom
+    box = img_mask.getbbox()
+    box_center = [(box[0]+box[2])//2, (box[1]+box[3])//2]
+    gap = max(image_np.shape)*0.02
+    edge_size = (max(box[2]-box[0], box[3]-box[1])+gap)//2
+    box_tuned = [box_center[0]-edge_size, box_center[1]-edge_size, box_center[0]+edge_size, box_center[1]+edge_size]
+    img_cropped = img_composite.transform((128, 128), Image.Transform.EXTENT, box_tuned, Image.Resampling.BILINEAR)
+    mask_cropped = img_mask.transform((128, 128), Image.Transform.EXTENT, box_tuned, Image.Resampling.BILINEAR)
+
+    aaa = image_name.split(".")
+    bbb = aaa[0:-1]
+    imidx = bbb[0]
+    for i in range(1, len(bbb)):
+        imidx = imidx + "." + bbb[i]
+
+    img_composite.save(os.path.join(d_dir, imidx + '.png'))
+    img_cropped.save(os.path.join(o_dir, imidx + '_a.png'))
+    mask_cropped.save(os.path.join(o_dir, imidx + '_m.png'))
+
+
+def save_output(image_name, pred, d_dir, threshold=128):
     predict = pred
     predict = predict.squeeze()
     predict_np = predict.cpu().data.numpy()
@@ -41,7 +78,10 @@ def save_output(image_name, pred, d_dir):
     image = io.imread(image_name)
     imo = im.resize((image.shape[1], image.shape[0]), resample=Image.BILINEAR)
 
-    pb_np = np.array(imo)
+    mask = np.array(imo)
+    img_rgb = np.zeros(mask.shape, dtype=np.uint8)
+    img_rgb[mask > threshold] = image[mask > threshold]
+    img_rgb = Image.fromarray(img_rgb, mode='RGB')
 
     aaa = img_name.split(".")
     bbb = aaa[0:-1]
@@ -49,7 +89,12 @@ def save_output(image_name, pred, d_dir):
     for i in range(1, len(bbb)):
         imidx = imidx + "." + bbb[i]
 
-    imo.save(os.path.join(d_dir, imidx+'.png'))
+    if not os.path.exists(os.path.join(d_dir, 'mask')):
+        os.mkdir(os.path.join(d_dir, 'mask'))
+    if not os.path.exists(os.path.join(d_dir, 'rgb')):
+        os.mkdir(os.path.join(d_dir, 'rgb'))
+    imo.save(os.path.join(d_dir, 'mask', imidx+'.png'))
+    img_rgb.save(os.path.join(d_dir, 'rgb', imidx+'.png'))
 
 
 class U2net:
@@ -57,12 +102,16 @@ class U2net:
         self.model_name = default_model
 
         self.image_dir = ''
+        self.temp_dir = ''
         self.result_dir = ''
         self.model_dir = os.path.join(current_path, 'saved_models', self.model_name, self.model_name + '.pth')
 
         self.dataloader = None
         self.net = None
         self.img_name_list = []
+
+        self.total_images = -1
+        self.current_idx = -1
 
     def load_model(self):
         if self.model_name == 'u2net':
@@ -81,6 +130,8 @@ class U2net:
     def load_images(self):
         self.img_name_list = glob.glob(self.image_dir + os.sep + '*')
         print(self.img_name_list)
+        self.total_images = len(self.img_name_list)
+        self.current_idx = 0
 
         dataset = SalObjDataset(img_name_list=self.img_name_list,
                                 lbl_name_list=[],
@@ -95,15 +146,20 @@ class U2net:
     def change_image_dir(self, image_dir: str):
         self.image_dir = image_dir
 
+    def change_temp_dir(self, temp_dir: str):
+        if not os.path.exists(temp_dir):
+            os.makedirs(temp_dir)
+        self.temp_dir = temp_dir
+
     def change_result_dir(self, result_dir: str):
         if not os.path.exists(result_dir):
             os.makedirs(result_dir)
         self.result_dir = result_dir
 
-    def process(self):
+    def process(self, callback=None):
         self.load_images()
         for i_test, data_test in enumerate(self.dataloader):
-
+            self.current_idx = i_test
             print("inferencing:", self.img_name_list[i_test].split(os.sep)[-1])
 
             inputs_test = data_test['image']
@@ -121,17 +177,24 @@ class U2net:
                 pred = d1[:, 0, :, :]
                 pred = normPRED(pred)
 
-                # save results to test_results folder
-                if not os.path.exists(self.result_dir):
-                    os.makedirs(self.result_dir, exist_ok=True)
-                save_output(self.img_name_list[i_test], pred, self.result_dir)
+                # save_output(self.img_name_list[i_test], pred, self.result_dir)
+                save_output_overlay(self.img_name_list[i_test], pred, self.temp_dir, self.result_dir)
 
             del d1, d2, d3, d4, d5, d6, d7
+
+            if callback is not None:
+                callback()
+
+        self.current_idx = self.total_images
+
+        if callback is not None:
+            callback()
 
 
 if __name__ == '__main__':
     u2net = U2net()
     u2net.load_model()
     u2net.change_image_dir(os.path.join(current_path, 'test_data', 'test_images'))
+    u2net.change_temp_dir(os.path.join(current_path, 'test_data', 'temp_images'))
     u2net.change_result_dir(os.path.join(current_path, 'test_data', 'u2netp_results'))
     u2net.process()
--- main.py
+++ main.py
@@ -2,6 +2,20 @@ import dearpygui.dearpygui as dpg
 from U2Net.u2net_wrapper import U2net
 
 
+def config_parser():
+    import configargparse
+    parser = configargparse.ArgumentParser()
+
+    parser.add_argument('--config', is_config_file=True,
+                        help='config file path')
+
+    parser.add_argument('--inputdir', type=str)
+    parser.add_argument("--tempdir", type=str)
+    parser.add_argument("--resultdir", type=str)
+
+    return parser
+
+
 class U2netGui:
     def __init__(self):
         self.u2net = U2net()
@@ -10,28 +24,66 @@ class U2netGui:
         dpg.create_context()
         self.register_dpg()
 
-        dpg.start_dearpygui()
-
-    def register_dpg(self):
-        dpg.add_file_dialog(directory_selector=True, show=False, width=600, height=300,
-                            callback=lambda _, app_data: self.u2net.change_image_dir(app_data['file_path_name']),
-                            tag="img_path_dialog_id")
-        dpg.add_file_dialog(directory_selector=True, show=False, width=600, height=300,
-                            callback=lambda _, app_data: self.u2net.change_result_dir(app_data['file_path_name']),
-                            tag="result_path_dialog_id")
-
-        with dpg.window(label="U2net-Segmentation"):
-            with dpg.group(horizontal=True):
-                dpg.add_text("Source path:")
-                dpg.add_button(label="Select", callback=lambda: dpg.show_item("img_path_dialog_id"))
-            with dpg.group(horizontal=True):
-                dpg.add_text("Result path:")
-                dpg.add_button(label="Select", callback=lambda: dpg.show_item("result_path_dialog_id"))
-            dpg.add_button(label="Process", callback=lambda: self.u2net.process())
-
         dpg.create_viewport()
         dpg.setup_dearpygui()
         dpg.show_viewport()
+
+        parser = config_parser()
+        args = parser.parse_args()
+
+        if args.inputdir is not None and \
+           args.tempdir is not None and \
+           args.resultdir is not None:
+            print("Load config file successfully!")
+            dpg.set_value("it_input_path", args.inputdir)
+            dpg.set_value("it_temp_path", args.tempdir)
+            dpg.set_value("it_result_path", args.resultdir)
+
+        dpg.start_dearpygui()
+
+    def update_path(self):
+        self.u2net.change_image_dir(dpg.get_value("it_input_path"))
+        self.u2net.change_temp_dir(dpg.get_value("it_temp_path"))
+        self.u2net.change_result_dir(dpg.get_value("it_result_path"))
+
+    def register_dpg(self):
+        with dpg.value_registry():
+            dpg.add_float_value(default_value=0, tag="sf_processing")
+            dpg.add_string_value(default_value="/", tag="it_input_path")
+            dpg.add_string_value(default_value="/", tag="it_temp_path")
+            dpg.add_string_value(default_value="/", tag="it_result_path")
+
+        dpg.add_file_dialog(directory_selector=True, show=False, width=600, height=300,
+                            callback=lambda _, app_data: dpg.set_value("it_input_path", app_data['file_path_name']),
+                            tag="img_path_dialog_id")
+        dpg.add_file_dialog(directory_selector=True, show=False, width=600, height=300,
+                            callback=lambda _, app_data: dpg.set_value("it_temp_path", app_data['file_path_name']),
+                            tag="temp_path_dialog_id")
+        dpg.add_file_dialog(directory_selector=True, show=False, width=600, height=300,
+                            callback=lambda _, app_data: dpg.set_value("it_result_path", app_data['file_path_name']),
+                            tag="result_path_dialog_id")
+
+        with dpg.window(label="U2net-Segmentation", width=600):
+            with dpg.group(horizontal=True):
+                dpg.add_input_text(label="input images path", source="it_input_path")
+                dpg.add_button(label="Select", callback=lambda: dpg.show_item("img_path_dialog_id"))
+
+            with dpg.group(horizontal=True):
+                dpg.add_input_text(label="temp images path", source="it_temp_path")
+                dpg.add_button(label="Select", callback=lambda: dpg.show_item("temp_path_dialog_id"))
+
+            with dpg.group(horizontal=True):
+                dpg.add_input_text(label="result images path", source="it_result_path")
+                dpg.add_button(label="Select", callback=lambda: dpg.show_item("result_path_dialog_id"))
+
+            def btn_callback():
+                self.update_path()
+                self.u2net.process(callback=lambda:
+                                   dpg.set_value("sf_processing", self.u2net.current_idx/self.u2net.total_images*100))
+
+            with dpg.group(horizontal=True):
+                dpg.add_button(label="Process", callback=btn_callback)
+                dpg.add_slider_float(source="sf_processing")
 
     def __del__(self):
         dpg.destroy_context()
