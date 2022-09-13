b8105cc071ad79bd352f95304eac9abfbb2d97df
ar-mine <934218777@qq.com>
2022-09-13 16:03:33 +0800

    fix:Change path input to readonly. Select directs to wrong column. Cannot open path with space.

--- pygui.py
+++ pygui.py
@@ -1,13 +1,11 @@
 import os
-import glob
-import cv2
 import shutil
-from sys import platform
-from skimage.io import imsave
 import omegaconf
 from omegaconf import DictConfig
-from typing import Optional
 import dearpygui.dearpygui as dpg
+from sys import platform
+
+from utils import video2image
 from U2Net.u2net_wrapper import U2net
 
 # Names of Folders that will be used in the project
@@ -58,83 +56,103 @@ class U2netGui:
         print("Finish!")
 
     def register_dpg(self):
-        # Register file dialog
+        def set_value_creator(key_):
+            path_id = "{}_path".format(key_)
+            return lambda _, app_data: dpg.set_value(path_id, app_data['file_path_name'])
+
+        # Register file path dialog
         for key in PathEnum:
             dpg.add_file_dialog(directory_selector=True, show=False, width=600, height=300,
-                                callback=lambda _, app_data: dpg.set_value("{}_path".format(key),
-                                                                           app_data['file_path_name']),
-                                tag="{}_path_dialog_id".format(key), default_path=dpg.get_value("{}_path".format(key)))
+                                callback=set_value_creator(key),
+                                tag="{}_path_dialog_id".format(key))
 
         # Register widgets in window
         with dpg.window(label="Template-Generation", width=600):
-            def separate_callback():
-                dpg.set_value("separate_success", "Separating")
+            # For button 'Select'
+            def dialog_creator(key_):
+                """
+                Return a dialog callback
+                """
+                dialog_tag_ = "{}_path_dialog_id".format(key_)
+                return lambda: dpg.show_item(dialog_tag_)
 
-                # Start to process images
-                video2image(dpg.get_value("video_path"), dpg.get_value("input_path"),
-                            interval=dpg.get_value("separate_rate"))
-                dpg.set_value("separate_success", "Complete!")
+            # For button 'Open'
+            def open_creator(key_):
+                """
+                Given dpg context and the prefix of path, it will generate a function according to OS
+                as a callback for button to bind with.
+                """
+                path_id = "{}_path".format(key_)
 
-            def open_creator(path):
-                def creator_win():
-                    os.system("explorer {}".format(path))
+                def func():
+                    path = dpg.get_value(path_id)
 
-                def creator_linux():
-                    os.system("xdg-open {}".format(path))
+                    if platform == "linux" or platform == "linux2":
+                        os.system("xdg-open \"{}\"".format(path))
+                    elif platform == "darwin":
+                        pass
+                    elif platform == "win32":
+                        os.system("explorer {}".format(path))
 
-                if platform == "linux" or platform == "linux2":
-                    return creator_linux
-                elif platform == "darwin":
-                    pass
-                elif platform == "win32":
-                    return creator_win
+                return func
 
             for key in PathEnum:
                 with dpg.group(horizontal=True):
-                    dpg.add_input_text(source="{}_path".format(key), width=350)
-                    dpg.add_button(label="Select", callback=lambda: dpg.show_item("{}_path_dialog_id".format(key)))
-                    open_temp = open_creator(dpg.get_value("{}_path".format(key)))
-                    dpg.add_button(label="Open", callback=open_temp)
+                    dpg.add_input_text(source="{}_path".format(key), width=350, readonly=True)
+                    dpg.add_button(label="Select", callback=dialog_creator(key))
+                    dpg.add_button(label="Open", callback=open_creator(key))
                     dpg.add_text("{} folder".format(key))
+                # Video folder's special option
                 if key == "video":
+                    # For button 'Separate'
+                    def separate_callback():
+                        dpg.set_value("separate_success", "Separating")
+
+                        # Start to process images
+                        video2image(dpg.get_value("video_path"), dpg.get_value("input_path"),
+                                    interval=dpg.get_value("separate_rate"))
+                        dpg.set_value("separate_success", "Complete!")
+
                     with dpg.group(horizontal=True):
                         dpg.add_button(label="Separate", callback=separate_callback, width=60)
                         dpg.add_slider_int(source="separate_rate", min_value=1, max_value=30, width=282)
                         dpg.add_text(tag="separate_success")
 
-            def process_callback():
-                dpg.set_value("process_success", "")
-
-                # Write paths saved in GUI to U2Net model class
-                self.u2net.change_image_dir(dpg.get_value("input_path"))
-                self.u2net.change_temp_dir(dpg.get_value("temp_path"))
-                self.u2net.change_result_dir(dpg.get_value("result_path"))
-
-                # Start to process images
-                self.u2net.process(callback=lambda:
-                dpg.set_value("processing_rate",
-                              self.u2net.current_idx / self.u2net.total_images * 100)
-                                   )
-                dpg.set_value("process_success", "Complete!")
-
             with dpg.group(horizontal=True):
+                # For button 'Process'
+                def process_callback():
+                    dpg.set_value("process_success", "")
+
+                    # Write paths saved in GUI to U2Net model class
+                    self.u2net.change_image_dir(dpg.get_value("input_path"))
+                    self.u2net.change_temp_dir(dpg.get_value("temp_path"))
+                    self.u2net.change_result_dir(dpg.get_value("result_path"))
+
+                    # Start to process images
+                    self.u2net.process(callback=lambda:
+                    dpg.set_value("processing_rate",
+                                  self.u2net.current_idx / self.u2net.total_images * 100)
+                                       )
+                    dpg.set_value("process_success", "Complete!")
+
                 dpg.add_button(label="Process", callback=process_callback, width=60)
                 dpg.add_slider_float(source="processing_rate", width=282)
                 dpg.add_text(tag="process_success")
 
-            def btn_clean():
-                # Only clean folders that keep checkbox
-                for _key in PathEnum:
-                    if dpg.get_value("cb_{}".format(_key)):
-                        path_temp = dpg.get_value("{}_path".format(_key))
-                        if os.path.exists(path_temp):
-                            shutil.rmtree(path_temp)
-                            os.mkdir(path_temp)
-                        else:
-                            print("{} path does not exist!")
-                dpg.set_value("t_clean", "Clean finish!")
-
             with dpg.group(horizontal=True):
+                # For button 'clean'
+                def btn_clean():
+                    # Only clean folders that keep checkbox
+                    for _key in PathEnum:
+                        if dpg.get_value("cb_{}".format(_key)):
+                            path_temp = dpg.get_value("{}_path".format(_key))
+                            if os.path.exists(path_temp):
+                                shutil.rmtree(path_temp)
+                                os.mkdir(path_temp)
+                            else:
+                                print("{} path does not exist!")
+                    dpg.set_value("t_clean", "Clean finish!")
+
                 dpg.add_button(label="Clean", callback=btn_clean, width=60)
 
                 # Add checkboxes for cleaning
@@ -149,37 +167,4 @@ class U2netGui:
         dpg.destroy_context()
 
 
-def video2image(input_dir, output_dir, interval=30, image_size: Optional[int] = None, transpose=False):
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
-    video_name_list = glob.glob(input_dir + os.sep + '*.mp4')
-    count = 0
-    for input_video in video_name_list:
-        print(f'split video {input_video} into images ...')
-        video_cap = cv2.VideoCapture(input_video)
-        success, image = video_cap.read()
-        while success:
-            if count % interval == 0:
-                # If images need to be scaled
-                if image_size is not None:
-                    h, w = image.shape[:2]
-                    ratio = image_size / max(h, w)
-                    ht, wt = int(ratio * h), int(ratio * w)
-                    image = cv2.resize(image, (wt, ht), interpolation=cv2.INTER_LINEAR)
 
-                # If images need to be transposed
-                if transpose:
-                    v0 = cv2.getVersionMajor()
-                    v1 = cv2.getVersionMinor()
-                    if v0 >= 4 and v1 >= 5:
-                        image = cv2.flip(image, 0)
-                        image = cv2.flip(image, 1)
-                    else:
-                        image = cv2.transpose(image)
-                        image = cv2.flip(image, 1)
-
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                imsave(f"{output_dir}/frame%d.jpg" % count, image)  # save frame as JPEG file
-            success, image = video_cap.read()
-            count += 1
-    return count
---
+++ utils.py
@@ -0,0 +1,41 @@
+import os
+import glob
+from typing import Optional
+import cv2
+from skimage.io import imsave
+
+
+def video2image(input_dir, output_dir, interval=30, image_size: Optional[int] = None, transpose=False):
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    video_name_list = glob.glob(input_dir + os.sep + '*.mp4')
+    count = 0
+    for input_video in video_name_list:
+        print(f'split video {input_video} into images ...')
+        video_cap = cv2.VideoCapture(input_video)
+        success, image = video_cap.read()
+        while success:
+            if count % interval == 0:
+                # If images need to be scaled
+                if image_size is not None:
+                    h, w = image.shape[:2]
+                    ratio = image_size / max(h, w)
+                    ht, wt = int(ratio * h), int(ratio * w)
+                    image = cv2.resize(image, (wt, ht), interpolation=cv2.INTER_LINEAR)
+
+                # If images need to be transposed
+                if transpose:
+                    v0 = cv2.getVersionMajor()
+                    v1 = cv2.getVersionMinor()
+                    if v0 >= 4 and v1 >= 5:
+                        image = cv2.flip(image, 0)
+                        image = cv2.flip(image, 1)
+                    else:
+                        image = cv2.transpose(image)
+                        image = cv2.flip(image, 1)
+
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                imsave(f"{output_dir}/frame%d.jpg" % count, image)  # save frame as JPEG file
+            success, image = video_cap.read()
+            count += 1
+    return count
